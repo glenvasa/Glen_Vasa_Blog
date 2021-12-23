ef0a5ca601c6a142579857937d154f3837171a49
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-23 12:05:22 -0500

    Create [slug].js

---
+++ pages/posts/[slug].js
@@ -0,0 +1,8 @@
+// slug is a human readable identifier such as blog title with dashes separating words; 
+// better for seo than if we just used post id as identifier
+
+function PostDetailPage(){
+
+}
+
+export default PostDetailPage
