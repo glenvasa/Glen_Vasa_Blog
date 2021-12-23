3ae0a0fd699a903b074403935120e1a776e0cc83
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-23 16:53:51 -0500

    displaying PostContent component on PostDetailPage

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,8 +1,12 @@
 // slug is a human readable identifier such as blog title with dashes separating words; 
 // better for seo than if we just used post id as identifier
 
-function PostDetailPage(){
+import PostContent from "../../components/posts/post-detail/post-content"
 
+function PostDetailPage(){
+    return (
+        <PostContent />
+    )
 }
 
 export default PostDetailPage
