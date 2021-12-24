5d787d7da2d90e28d2a6eec544bd5939d45efcb2
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-24 13:37:41 -0500

    added customRenderers value for components prop in ReactMarkdownt to instruct Next that we want to use custom Next Image for images instead of default <img> element
    Next Image allows better customization, fetching of custom image sizes  which are cached for later use and default lazy loading.

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,16 +1,46 @@
 import ReactMarkdown from "react-markdown";
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
+import Image from 'next/image'
 
 function PostContent(props) {
   const { post } = props;
 
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+  // user renderers prop in ReactMarkdown to tell Next how we want to treat certain elements if found, instead of default behavior
+  // by default ReactMarkdown will treat an image found as regular <img> element
+  const customRenderers = {
+    // img(image) {
+    //     // image.alt text is already available as text in [] in md file next to image
+    //     return <Image src={`/images/posts/${post.slug}/${image.src}`} alt={image.alt} width={600} height={300}/>
+    // },
+    p(paragraph) {
+        const { node } = paragraph;
+        // we only want to override paragraph if it is an img paragraph (in markdown images rendered inside paragraphs by default)
+        if (node.children[0].tagName === 'img') {
+          const image = node.children[0];
+
+          return (
+            <div className={classes.image}>
+              <Image
+                src={`/images/posts/${post.slug}/${image.properties.src}`}
+                alt={image.alt}
+                width={600}
+                height={300}
+              />
+            </div>
+          );
+        }
+
+        return <p>{paragraph.children}</p>
+  }}
+
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
