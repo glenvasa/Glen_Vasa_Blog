1f06ae6619241fc082946abe74214ccb246a236b
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-25 13:14:01 -0500

    added logic to store messages in MongoDb

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import {MongoClient} from 'mongodb'
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body; // Next automatically parses body object
 
@@ -21,10 +23,32 @@ function handler(req, res) {
         message
     }
 
-    console.log(newMessage);
+    let client
 
+    try {
+    client = await MongoClient.connect('mongodb+srv://gv_blog:gv_blog@cluster0.icgtj.mongodb.net/my-site?retryWrites=true&w=majority')
+    } catch (error) {
+        res.status(500).json({message: 'Could not connect to database.'});
+        return
+    }
+
+    const db = client.db()
+    let result
+
+    try {
+     result = await db.collection('messages').insertOne(newMessage)
+     // don't need this code b/c result object automatically adds mongodb _id 
+     //  newMessage.id = result.insertedId
+    }  catch (error) {
+            client.close()
+            res.status(500).json({message: 'Something went wrong. Message not saved in database.'});
+            return
+    }
+    
+
+    client.close()
     res.status(201).json({message: 'Message successfully stored', newMessage: newMessage})
-
+   
   }
 }
 
