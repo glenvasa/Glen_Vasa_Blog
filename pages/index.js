66f48beba6c92266bc7a5dc335c00a0095810cc9
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-25 14:29:43 -0500

    Added Head component for page metadata

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import Head from 'next/head'
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from '../components/home-page/featured-posts'
 import {getFeaturedPosts} from '../lib/posts-util'
@@ -8,6 +9,10 @@ function HomePage(props) {
 
   return (
     <Fragment>
+      <Head>
+        <title>Welcome to Glen's Blog</title>
+        <meta name='description' content='I post about programming and web development' />
+      </Head>
       <Hero />
       <FeaturedPosts posts={posts}/>
     </Fragment>
