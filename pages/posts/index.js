ca90fe7272cf646bde9585aeb6c1e7fe41e0e5c7
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-25 14:29:52 -0500

    Added Head component for page metadata

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,10 +1,20 @@
 import AllPosts from "../../components/posts/all-posts"
 import {getAllPosts} from '../../lib/posts-util'
-
+import Head from 'next/head'
+import {Fragment} from 'react'
    
 
 function AllPostsPage(props){
-  return <AllPosts posts={props.posts}/>
+  return (
+    <Fragment>
+      <Head>
+        <title>All Blog Posts</title>
+        <meta name='description' content='A list of all programming-related blog posts' />
+      </Head>
+      <AllPosts posts={props.posts}/> 
+    </Fragment>
+ 
+    )
 }
 
 export function getStaticProps(){
